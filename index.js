f0fb502d95107648f9f7a6e379adf9fc7dcb98a9
Vasya010 <sdsasdsadsa5@gmail.com>
2025-09-28 18:41:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware для обработки JSON
+app.use(express.json());
+
+// Пример маршрута
+app.get('/', (req, res) => {
+    res.send('Привет, это твой бэкенд на Node.js!');
+});
+
+// Запуск сервера
+app.listen(port, () => {
+    console.log(`Сервер запущен на http://localhost:${port}`);
+});
